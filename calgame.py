a4baa1874a64a979bc17da11ae1e94bbe86cc89d
nonthaphonKU <nonthaphon.v@ku.th>
2022-11-15 15:04:39 +0700

    Add calgame to git

---
+++ calgame.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from random import randint 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    a = randint(1,100)
+    b = randint(1,100)
+    return render_template('index.html',
+        a=a, 
+        b=b
+    )
+
+@app.route('/check', methods=['POST'])
+def check_answer():
+    a = int(request.form.get('a','0'))
+    b = int(request.form.get('b','0'))
+    try :
+            answer = int(request.form.get('answer','0'))
+            if a + b == answer:
+                cresult = 'ขอแสดงความยินดี คุณตอบถูก!'
+            else:
+                cresult = f'เสียใจ คุณตอบผิด!คำตอบที่ถูกต้องคือ {a} + {b} = {a+b}'
+    except ValueError:
+            cresult = 'ใส่คำตอบเป็นตัวเลขจำนวนเต็ม'
+    
+    return render_template('result.html', result=cresult)
+
+app.run(debug=True, port=8000)
+
+
+
+"""    if ((request.form.get('answer','0')) == '') or (((request.form.get('answer','0')).isdigit()) == False):
+        answer = 0
+    else:
+        answer = int(request.form.get('answer','0'))
+"""
